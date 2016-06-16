af93e6d1e454eb4607e38f019fa982940af12667
Charly Attrait <c.attrait@iia-laval.fr>
2016-06-16 18:01:18 +0200

    FONCTIONNEMENT

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using StupidCalculatorLib;
 
 namespace Calculator
 {
@@ -20,9 +9,101 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static bool Erase;
+        public static Operator Operator;
+        public static Operation lastOperator;
+
         public MainWindow()
         {
             InitializeComponent();
+            Erase = false;
+        }
+
+        /// <summary>
+        /// Button click events
+        /// </summary>
+        /// <param name="sender">button clicked</param>
+        /// <param name="e"></param>
+        private void BT_Click(object sender, RoutedEventArgs e)
+        {
+            if (Operator == null)
+            {
+                Operator = new Operator();
+            }
+
+            // if erase = true, save current value and clear textbox
+            if (Erase)
+            {
+                TXT_CALCUL.Text = "";
+                Erase = false;
+            }
+
+            switch (((Button)sender).Content.ToString())
+            {
+                case "CE":
+                    TXT_CALCUL.Text = "";
+                    break;
+                case "C":
+                    TXT_CALCUL.Text = "";
+                    break;
+
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    // add number to textbox
+                    TXT_CALCUL.Text += ((Button)sender).Content.ToString();
+                    break;
+                case ",":
+                    // if textbox not already contains decimal, and minimum one number exist
+                    if (TXT_CALCUL.Text == "")
+                    {
+                        TXT_CALCUL.Text = "0";
+                    }
+                    if (!TXT_CALCUL.Text.Contains(","))
+                    {
+                        TXT_CALCUL.Text += ((Button)sender).Content.ToString();
+                    }
+                    break;
+
+                case "X":
+                    Operator.operation = Operation.MULTIPLY;
+                    Operator.addNumber(double.Parse(TXT_CALCUL.Text));
+                    TXT_CALCUL.Text += ((Button)sender).Content.ToString();
+                    Erase = true;
+                    break;
+                case "-":
+                    Operator.operation = Operation.SUBSTRACT;
+                    Operator.addNumber(double.Parse(TXT_CALCUL.Text));
+                    TXT_CALCUL.Text += ((Button)sender).Content.ToString();
+                    Erase = true;
+                    break;
+                case "+":
+                    Operator.operation = Operation.ADD;
+                    Operator.addNumber(double.Parse(TXT_CALCUL.Text));
+                    TXT_CALCUL.Text += ((Button)sender).Content.ToString();
+                    Erase = true;
+                    break;
+                case "/":
+                    Operator.operation = Operation.DIVIDE;
+                    Operator.addNumber(double.Parse(TXT_CALCUL.Text));
+                    TXT_CALCUL.Text += ((Button)sender).Content.ToString();
+                    Erase = true;
+                    break;
+                case "=":
+                    Operator.addNumber(double.Parse(TXT_CALCUL.Text));
+                    TXT_CALCUL.Text = Operator.compute().ToString();
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
